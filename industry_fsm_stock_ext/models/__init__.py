2c4ef63ab74d4021aa80449c812ecf8028589a17
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-12-14 16:10:11 -0500

    Update __init__.py

--- industry_fsm_stock_ext/models/__init__.py
+++ industry_fsm_stock_ext/models/__init__.py
@@ -3,6 +3,6 @@
 from . import project_task_recurrence
 from . import project_task_fsm
 from . import product
-from . import project
+#from . import project
 from . import sale_order
 from . import stock_move
