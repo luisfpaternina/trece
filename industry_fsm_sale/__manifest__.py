8535ca80a6bb80620a603afb9edae43842c675e4
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-12-14 15:36:53 -0500

    Update __manifest__.py

--- industry_fsm_sale/__manifest__.py
+++ industry_fsm_sale/__manifest__.py
@@ -2,12 +2,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': "Field Service - Sale",
+
     'summary': "Schedule and track onsite operations, invoice time and material",
+
     'description': """
 Create Sales order with timesheets and products from tasks
     """,
     'category': 'Services/Field Service',
+
     'version': '1.0',
+    
     'depends': ['industry_fsm', 'sale_timesheet_enterprise'],
     'qweb': [
         "static/src/xml/fsm_quantity.xml"
